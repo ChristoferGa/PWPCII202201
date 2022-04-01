5249c906b4b63e99a6937ded7614706b67447549
ChristoferGa <christofergallo1@gmail.com>
2022-04-01 15:54:30 -0600

    Proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Author Gallo Gutiérrez Christofer' });
+});
+
+/* First Exercise */
+router.get('/FE', function (req, res, next) {
+  res.send('👋 Welcome to fullstack web course./nThis is the first exercise');
+});
+
+/* Second Exercise */
+router.get('/SE', function (req, res, next) {
+  res.status(200).json({ message: 'This is the second exercise' });
+});
+
+module.exports = router;
