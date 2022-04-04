660ffe12ea93973de94a77179a1a83ca2d27d636
ChristoferGa <christofergallo1@gmail.com>
2022-04-04 14:03:02 -0500

    Instalación de rimraf

---
+++ dist-server/routes/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    author: 'Author Gallo Gutiérrez Christofer',
+    babel: 'Babel is running'
+  });
+});
+/* First Exercise */
+
+router.get('/FE', function (req, res, next) {
+  res.send('👋 Welcome to fullstack web course./nThis is the first exercise');
+});
+/* Second Exercise */
+
+router.get('/SE', function (req, res, next) {
+  res.status(200).json({
+    message: 'This is the second exercise'
+  });
+});
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import aboutRouter from './routes/about';
 
 var app = express();
 
